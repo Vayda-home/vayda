4f2efc95a24db3e262e2cc1e720389bf2ec55499
Jatin Gupta <38355753+Jatingupta-2@users.noreply.github.com>
2024-07-21 10:38:06 +0530

    changes

---
+++ scripts.js
@@ -0,0 +1,61 @@
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let errorMessages = "";
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const social_media = document.getElementById("social_media").value.trim();
+    const invitee_full_name = document
+      .getElementById("invitee_full_name")
+      .value.trim();
+
+    if (!name) {
+      errorMessages += "<p>Please enter your name.</p>";
+    }
+
+    if (!email) {
+      errorMessages += "<p>Please enter your email.</p>";
+    }
+
+    if (!phone) {
+      errorMessages += "<p>Please enter your phone number.</p>";
+    }
+
+    if (!social_media) {
+      errorMessages += "<p>Please enter your Social media handle!</p>";
+    }
+
+    if (!invitee_full_name) {
+      errorMessages += "<p>Please enter your Invitee full name!</p>";
+    }
+
+    if (errorMessages) {
+      document.getElementById("errorMessages").innerHTML = errorMessages;
+    } else {
+      // Here you can add further code to handle form submission
+      var obj = {
+        html: ` You have a Client form.<br><br> 
+        <h3>Details are</h3>
+        <ul style="list-style-type:none;">
+        <li>Name: ${name}<br></li>
+        <li>Email: ${email}<br></li>
+        <li>Phone: ${phone}<br></li>
+        <li>Social media handle: ${social_media}<br></li>
+        <li>Invitee full name: ${invitee_full_name}<br></li>
+        </ul>`,
+        subject: "Vayda form",
+      };
+
+      axios({
+        method: "post",
+        url: "https://backend.avmade.com/api/v1/products/rfq",
+        // url: "http://localhost:5000/api/v1/products/rfq",
+        data: obj,
+      }).then(function (response) {
+        document.getElementById("successMessages").innerHTML =
+          "<p>We have received your request! Thank you!!</p>";
+      });
+    }
+  });
